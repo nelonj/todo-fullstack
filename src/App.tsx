e33ed8ef8c2aea3f0eddc42d1d34a4e20f73411d
Olivia Neil-Jones <olivianeiljones@gmail.com>
2021-03-16 21:39:57 +0100

    set up front-end and initialised backend setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+interface IItem {
+  description: string
+}
+
+
 function App() {
+  const emptyList: IItem[] = []
+  const [list, setList] = useState(emptyList)
+  const [item, setItem] = useState('')
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> My ToDo List</h1>
+      
+      <input type='text' onChange={e => setItem(e.target.value)}></input>
+      <button onClick={() => {
+        const cloneList: IItem[] = [...list]
+        cloneList.push({description: item})
+        setList(cloneList)
+      }}> ADD </button>
+
+      <ol> {list.map((element, index) => <ListItem {...element} key={index}/>)}</ol>
     </div>
   );
+}
+
+function ListItem(props: IItem) {
+  const [completion, setCompletion] = useState(false)
+  console.log(props.description, completion)
+  
+  return (
+    <li> {props.description} 
+      <input type='checkbox' onChange={() => {if (completion === true) {setCompletion(false)} else {setCompletion(true)}}}></input>
+      <button onClick={() => console.log('edited')}> EDIT </button> 
+      <button onClick={() => console.log('deleted')}> DELETE </button>
+    </li>
+    
+  )
 }
 
 export default App;
