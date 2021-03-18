fa6f1f067d4aec69dcf75ce2a26bc6eff0f66b0f
Olivia Neil-Jones <olivianeiljones@gmail.com>
2021-03-18 13:34:36 +0100

    deleted unnecessary key state

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -12,17 +12,17 @@ interface IListItemProps {
 export function ListItem(props: IListItemProps) {
     const [completion, setCompletion] = useState(false)
     // console.log(props.description, completion)
-    const [key, setKey] = useState(props.id)
+   
     
     return (
-      <li key={key}> {props.description} 
+      <li key={props.id}> {props.description} 
         <input type='checkbox' onChange={() => {if (completion === true) {setCompletion(false)} else {setCompletion(true)}}}></input>
         <button 
           // key={props.id} 
           onClick={() => console.log('edited')}> EDIT </button> 
         <button 
           // key={props.id} 
-          onClick={() => props.deleteHandler(key)}> DELETE </button>
+          onClick={() => props.deleteHandler(props.id)}> DELETE </button>
       </li>
       
     )
